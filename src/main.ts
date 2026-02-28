13216e3cd81f75655443205540161016ed01d858
Fernando Lucas Vieira Junior <fl.vieirajr@gmail.com>
2026-02-28 20:00:39 -0300

    chore(main): Correção de erros no-floating-promises e melhora no acabamento das linhas ESLint.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Erro ao iniciar a aplicação:', err);
+  process.exit(1);
+});
